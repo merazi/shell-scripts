74878cff913203e9f553b98248522ceb6ddd689e
Merazi <merazi_pub@riseup.net>
2021-12-20 21:32:24 -0600

    Script for lemonbar

--- lemonbar-script.sh
+++ lemonbar-script.sh
@@ -3,34 +3,34 @@
 Cal()
 {
     DATE=$(date "+%A, %d %B %Y - %R")
-        echo -e -n "\uf073 ${DATE}"
+    echo -e -n "\uf073 ${DATE}"
 }
 
-Workspace() 
+Workspace()
 {
-# get the current desktop number, add 1 because
-# xdotool outputs from 0 to 9
+    # get the current desktop number, add 1 because
+    # xdotool outputs from 0 to 9
     echo -e "$(( $(xdotool get_desktop) + 1 ))"
 }
 
 ActiveWindow()
 {
     len=$(echo -n "$(xdotool getwindowfocus getwindowname)" | wc -m)
-        max_len=40
-        if [ "$len" -gt "$max_len" ]; then
-            echo -n "$(xdotool getwindowfocus getwindowname | cut -c 1-$max_len)..."
-        else
-            echo -n "$(xdotool getwindowfocus getwindowname)"
-                fi
+    max_len=40
+    if [ "$len" -gt "$max_len" ]; then
+	echo -n "$(xdotool getwindowfocus getwindowname | cut -c 1-$max_len)..."
+    else
+	echo -n "$(xdotool getwindowfocus getwindowname)"
+    fi
 }
 
-Battery() 
+Battery()
 {
     echo -e "$(acpi --battery | awk -F, '{gsub(/ /,""); print $2}')"
 }
 
 while true; do
-    echo -e "%{l}%{F#00FF00}[$(Workspace)/10] %{F-}$(ActiveWindow)" \
-         "%{r}[$(battery.sh) | $(date.sh) | $(time.sh)]";
-         sleep 0.05s;
+    echo -e "%{l}%{F#00FF00}[$(Workspace)/4] %{F-}$(ActiveWindow)" \
+	 "%{r}[$(battery.sh) | $(date.sh) | $(time.sh)]";
+    sleep 0.05s;
 done
