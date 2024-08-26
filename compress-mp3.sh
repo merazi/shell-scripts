5c272fcab1c847e62817cecd82c98a9a8bcbf935
Merazi <jlmg3543@gmail.com>
2024-08-25 18:47:05 -0600

    Added compress-mp3.sh
    Script to compress all mp3 files in a given directory. It will create a
    directory called "opt" in that given directory and store all the compressed files there.

---
+++ compress-mp3.sh
@@ -0,0 +1,55 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: compress-mp3.sh
+# 
+#         USAGE: ./compress-mp3.sh [directory]
+# 
+#   DESCRIPTION: Compresses all mp3 files in PWD, creates a new directory named
+#   "comp" and saves the compressed files there
+# 
+#       OPTIONS: N/A
+#  REQUIREMENTS: lame
+#        AUTHOR: merazi, 
+#  ORGANIZATION: 
+#       CREATED: 08/24/2024 08:42
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+# if no directory entered then use PWD
+if [ -z "$1" ]; then
+    working_directory=$PWD
+else
+    working_directory=$1
+fi
+
+pushd "$working_directory"
+
+function create_directory_if_it_doesnt_exist() {
+    # longest function name ever
+    if [ -d "./comp" ]; then
+        read -p "comp directory already exists! Overwrite?" -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            rm -rf ./comp -v
+        fi
+    fi
+    mkdir ./comp
+}
+
+function convert_files() {
+    echo "Converting files in $working_directory"
+    for file in *.mp3; do
+        file_name=$(basename -- "$file")
+        new_name="${file_name%.*}-compressed.mp3"
+        lame -b 128 "$file" ./comp/"$new_name"
+    done
+    echo "Done!"
+}
+
+# first create the dictory
+create_directory_if_it_doesnt_exist
+
+# and then compress the files
+convert_files
