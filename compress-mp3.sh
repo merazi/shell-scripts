548f59cf58d7eefd663267232d5c4969db5b8c38
Merazi <jlmg3543@gmail.com>
2024-08-25 22:50:38 -0600

    Minor improvements

--- compress-mp3.sh
+++ compress-mp3.sh
@@ -26,28 +26,38 @@ pushd "$working_directory"
 
 function create_directory_if_it_doesnt_exist() {
     # longest function name ever
-    if [ -d "./comp" ]; then
-        read -p "comp directory already exists! Overwrite?" -n 1 -r
+    if [ -d "./$1" ]; then
+        read -p "$1 directory already exists! Overwrite?" -n 1 -r
         echo
         if [[ $REPLY =~ ^[Yy]$ ]]; then
-            rm -rf ./comp -v
+            rm -rf "./$1" -v
         fi
     fi
-    mkdir ./comp
+    mkdir "./$1"
 }
 
 function convert_files() {
-    echo "Converting files in $working_directory"
+    echo "Compressing files in $working_directory"
+    echo "Please wait..."
     for file in *.mp3; do
         file_name=$(basename -- "$file")
         new_name="${file_name%.*}-compressed.mp3"
-        lame -b 128 "$file" ./comp/"$new_name"
+        lame --silent -b 128 "$file" ./comp/"$new_name"
     done
-    echo "Done!"
 }
 
 # first create the dictory
-create_directory_if_it_doesnt_exist
+create_directory_if_it_doesnt_exist comp
 
 # and then compress the files
 convert_files
+
+# then backup the old files
+create_directory_if_it_doesnt_exist backup
+mv *.mp3 ./backup/
+
+# and put the compressed files inside current dir
+mv ./comp/* .
+rmdir ./comp
+
+echo "Done!"
